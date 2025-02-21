dcd778c9b37e5e86b92c44722176220592baaea3
tejendrauppal <tejendrauppal@gmail.com>
2025-02-21 10:16:36 +0000

    day2

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,22 @@
+"use strict";   
+// treat all js code as newer version
+
+// alert(3+3) we are using node.js not browser
+
+// code readability should be high
+
+let name = "tejendra"
+let age = "19"
+let isLoggedIn = false
+
+
+
+// number => 2 to the power 53
+// bigint
+// string => "" write in double quotes always
+// boolean => true or false
+// null => standalone value 
+// undefined => value is not defined here unlike null which itself is a value
+// symbol => for unique
+
+// object
