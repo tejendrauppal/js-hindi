6e2afe53eedb6192af8c02ae9e0dafd41edaeb49
tejendrauppal <tejendrauppal@gmail.com>
2025-02-24 07:43:24 +0000

    Day3

---
+++ 01_basics/05_string.js
@@ -0,0 +1,7 @@
+const name = "tejendra"
+const repoCount = 5
+
+// console.log(name + repoCount + " value");   this is the outdated way 
+
+console.log(`Hello my name is ${name} and my repocount is ${repoCount} `);
+
