be1b44b262ea85b5bfa1384cf7661d4a5a3d4a6d
tejendrauppal <tejendrauppal@gmail.com>
2025-02-24 05:47:09 +0000

    day3

---
+++ 01_basics/03_coversion.js
@@ -0,0 +1,59 @@
+let score = "33abc"
+
+// console.log(typeof score);
+// console.log(typeof (score));
+
+let valueInNumber = Number(score);
+// console.log(typeof valueInNumber);
+// console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1  ;  false => 0
+
+// let isLoggedIn = 1
+let isLoggedIn = "tejendra"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+// console.log(booleanIsLoggedIn);
+
+
+// 1 => true; 0 => false
+// "tejendra" => true
+// "" => false
+
+let someNumber = 56
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+
+
+//   ******************************** operations  ************************************
+
+let value = 56
+let negValue = -value
+// console.log(negValue);
+
+let str1 = "Hello"
+let str2 = " Tejendra"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log(1 + 2);
+// console.log(1 + 2 + "2");
+// console.log("1" + 2 + 2);
+
+// console.log((3 + 4) * 5 % 3);
+
+
+// console.log(+true);
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
