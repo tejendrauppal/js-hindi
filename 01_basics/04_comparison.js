11d4b412c08f610a502d2aedcce0174f8ef62fdd
tejendrauppal <tejendrauppal@gmail.com>
2025-02-24 06:19:07 +0000

    day3

---
+++ 01_basics/04_comparison.js
@@ -0,0 +1,15 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 != 1);
+// console.log(2 == 1);
+
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+//  while comparing need to make sure that datatypes are same
+
+
+console.log(null > 0);
+console.log(null >= 0);
+console.log(null == 0);
